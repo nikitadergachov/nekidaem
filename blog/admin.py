9f1a09f52dbeeb6a435856ebf2ffb9ee1f9ad850
nikitadergachov <nikitadergachov@gmail.com>
2017-07-03 17:19:27 +0300

    Create Models (Blog, Post, Subscriptions, AlreadyReadPost)

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog, Post, Subscriptions
+
+admin.site.register(Blog)
+admin.site.register(Post)
+admin.site.register(Subscriptions)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+    blog = models.ForeignKey(Blog)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subscriptions(models.Model):
+    class Meta:
+        unique_together = (("user", "blog"),)
+
+    user = models.ForeignKey(User)
+    blog = models.ForeignKey(Blog)
+
+    def __str__(self):
+        return self.user.username + " " + self.blog.name
+
+
+class AlreadyReadPost(models.Model):
+    class Meta:
+        unique_together = (("user", "post"),)
+
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+
+    def __str__(self):
+        return self.user.username + " " + self.post.name
